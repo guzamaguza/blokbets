83889430fe586d8a5e664520a756f870ca51d63f
guzamaguza <guzamaguza@gmail.com>
2023-09-26 23:53:37 -0700

    Added Form to Admin Page

--- src/component/Admin/Admin.js
+++ src/component/Admin/Admin.js
@@ -29,18 +29,76 @@ const Admin = () => {
 
         <p>
         
-              <h1 className='text-[30px] sm:leading-[50px] mb-6 text-white	 sm:text-[50px] font-semibold '>Admin Page</h1>
-              <button id="loadButton" className='sm:left-30 sm:right-30 text-white uppercase  rounded-none font-normal hover:bg-[#47a09a] outline-none py-2 px-[60px] text-[10px]  text-normal border-[1px] leading-[20px]  border-[#59DCD3]'> Refresh & Display Data</button>
-              <div id="dataContainer"></div>
-              <script src="script.js"> </script>
-              
+        <h1 className='text-[30px] sm:leading-[50px] mb-6 text-white	 sm:text-[50px] font-semibold '>Admin Page</h1>
+                
         </p>
         &nbsp
         <p>
         
+        <p>
+        <form>
+          <label className='text-[#59DCD3]' >
+            Team A: 
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </p>
 
-        <button className='sm:left-30 sm:right-30 text-white uppercase  rounded-none font-normal hover:bg-[#47a09a] outline-none py-2 px-[60px] text-[10px]  text-normal border-[1px] leading-[20px]  border-[#59DCD3]'> Push Data to Refresh Contracts</button>
-              
+        &nbsp
+        <p>
+        <form>
+          <label className='text-[#59DCD3]' >
+            Team B: 
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </p>
+
+        &nbsp
+        <p>
+        <form>
+          <label className='text-[#59DCD3]' >
+            Ending Time: 
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </p>
+
+        &nbsp
+        <p>
+        <form>
+          <label className='text-[#59DCD3]' >
+            Closing Time: 
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </p>
+
+        &nbsp
+        <p>
+        <form>
+          <label className='text-[#59DCD3]' >
+            Min Bet: 
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </p>
+          
+
+
+
+        <p>
+          <br></br>
+          <button className='sm:left-30 sm:right-30 text-white uppercase  rounded-none font-normal hover:bg-[#47a09a] outline-none py-2 px-[60px] text-[10px]  text-normal border-[1px] leading-[20px]  border-[#59DCD3]'> Start Bet </button>
+          &nbsp
+          <button className='sm:left-30 sm:right-30 text-white uppercase  rounded-none font-normal hover:bg-[#47a09a] outline-none py-2 px-[60px] text-[10px]  text-normal border-[1px] leading-[20px]  border-[#59DCD3]'> Set Team Winner & End Bet </button>
+        </p>
+                   
               
         </p>
 
